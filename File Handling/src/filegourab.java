3434ce3a5be3062356c98cb49eef9c85af273cae
gourabofficial <gourabofficial702@gmail.com>
2024-06-21 10:36:43 +0530

    exception handeling

--- File Handling/src/filegourab.java
+++ File Handling/src/filegourab.java
@@ -3,12 +3,12 @@ import java.io.IOException;
 
 public class filegourab {
     public static void main(String[] args) {
-        String filename = "gourab.text";
+        String filename = "debesh.text";
         try{
             FileWriter file = new FileWriter(filename);
-            file.write("exam start ...");
+            file.write("about debesh: ");
             for(int i = 0; i <= 10; i++)
-                file.write("\n debesh is the best deffender in our clg ");
+                file.write("\nDebesh is the nigga man ");
             file.flush();
             System.out.println("file create succesfully: " +filename);
         }catch(IOException e){
