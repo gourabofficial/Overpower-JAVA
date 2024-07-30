09202ec9921b515ba41813c19a8c407e47367f3e
gourabofficial <gangulygourab3@gmail.com>
2024-07-30 19:59:13 +0530

    Add Threading

---
+++ Multithreading/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 1; i <= 1000; i++) {
+            System.out.printf("%d* ", i);
+        }
+
+        System.out.println();
+        for (int i = 1; i <= 1000; i++) {
+            System.out.printf("%d# ", i);
+        }
+        System.out.println();
+        for (int i = 1; i <= 1000; i++) {
+            System.out.printf("%d& ", i);
+        }
+        System.out.println();
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Time taken: " + (endTime - startTime) + "ms");
+    }
+}
