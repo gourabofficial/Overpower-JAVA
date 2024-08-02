0dfe87ca5a3c861b7f3262a060ba416e1be2f62f
gourabofficial <gangulygourab3@gmail.com>
2024-08-02 08:48:43 +0530

    update

--- Multithreading/src/threadPriority/Printtask.java
+++ Multithreading/src/threadPriority/Printtask.java
@@ -1,2 +1,19 @@
-package threadPriority;public class Printtask {
+package threadPriority;
+
+public class Printtask implements Runnable{
+
+
+
+    @Override
+    public void run() {
+        for(int i = 1; i <= 5; i++){
+            System.out.println(ch + " " + i);
+        }
+    }
+
+
+    private final String ch;
+    public Printtask(String c) {
+        this.ch = c;
+    }
 }
--- Multithreading/src/threadPriority/testPriority.java
+++ Multithreading/src/threadPriority/testPriority.java
@@ -1,2 +1,21 @@
-package threadPriority;public class testPriority {
+package threadPriority;
+
+public class testPriority {
+    public static void main(String[] args) {
+        Printtask task1 = new Printtask("task 1: a");
+        Printtask task2 = new Printtask("task 2: b");
+        Printtask task3 = new Printtask("task 3: c");
+
+        Thread t1 = new Thread(task1);
+        t1.setPriority(Thread.MIN_PRIORITY);
+        t1.start();
+
+        Thread t2 = new Thread(task2);
+        t2.setPriority(Thread.NORM_PRIORITY);
+        t2.start();
+
+        Thread t3 = new Thread(task3);
+        t3.setPriority(Thread.MAX_PRIORITY);
+        t3.start();
+    }
 }
