79c1f44c28a871d13110e0152861fbfaadb93350
gourabofficial <gangulygourab3@gmail.com>
2024-08-03 00:40:09 +0530

    complete Multithreading,Special Thanks to Sourabh sir

--- Multithreading/src/SynchronizedTest/mysirg/MultiThreading.java
+++ Multithreading/src/SynchronizedTest/mysirg/MultiThreading.java
@@ -4,25 +4,38 @@ import java.util.Scanner;
 
 public class MultiThreading {
     public static void main(String[] args) {
-        Account acc = new Account(1000,"Rahul");
+        Account acc = new Account(1000);
 
-        Customer c1 = new Customer(acc);
-        Customer c2 = new Customer(acc);
+        Customer c1 = new Customer(acc,"rahul");
+        Customer c2 = new Customer(acc,"kiran");
+        Customer c3 = new Customer(acc,"sujoy");
+        Customer c4 = new Customer(acc,"saheb");
+        Customer c5 = new Customer(acc,"raja");
+        Customer c6 = new Customer(acc,"papu");
 
         Thread t1 = new Thread(c1);
         Thread t2 = new Thread(c2);
+        Thread t3 = new Thread(c3);
+        Thread t4 = new Thread(c4);
+        Thread t5 = new Thread(c5);
+        Thread t6 = new Thread(c6);
+
         t1.start();
         t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+        t6.start();
 
     }
 }
 
 class Account{
-    private String name;
+
     private int balence;
-    public Account(int balence,String name){
+    public Account(int balence){
         this.balence = balence;
-        this.name = name;
+
     }
     public boolean isSufficientBalence(int w){
         if(balence >= w){
@@ -38,20 +51,27 @@ class Account{
     }
 }
 class Customer implements Runnable{
+    private String name;
     private Account acc;
-   public Customer(Account acc){
+   public Customer(Account acc,String name){
         this.acc = acc;
+        this.name = name;
     }
     @Override
     public void run(){
         Scanner sc = new Scanner(System.in);
-        System.out.println(" Enter your withdraw ammount: ");
-        int ammount = sc.nextInt();
 
-        if(acc.isSufficientBalence((ammount))){
-            acc.withdraw(ammount);
-        }else{
-            System.out.println("Insufficient balence");
+
+        synchronized (acc) {
+            System.out.println( name + " Enter your withdraw amount: ");
+            int ammount = sc.nextInt();
+
+            if (acc.isSufficientBalence((ammount))) {
+                System.out.println(name);
+                acc.withdraw(ammount);
+            } else {
+                System.out.println("Insufficient balence");
+            }
         }
     }
 }
